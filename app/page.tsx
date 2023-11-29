d57ebc566c1e1df90874d22f695e400edc0495f1
cheezeman <pksam5046@naver.com>
2023-11-29 17:43:28 +0900

    WIP:메인페이지 DB연결문 ts문법으로 수정 필요

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
+import { connectDB } from "@/util/database";
+import { MongoClient, Db } from "mongodb";
+
+export default async function Home() {
+  const client: MongoClient = (await connectDB) as MongoClient; // 'as MongoClient'를 사용하여 타입 명시
+  const db: Db = client.db("forum");
+  const result = await db.collection("post").find().toArray();
+  console.log(result);
+
   return <main></main>;
 }
