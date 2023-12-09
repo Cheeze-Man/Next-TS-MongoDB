86cfb0ece4c9fa88d4eae67e69bfb7a5c88bf89a
cheezeman <pksam5046@naver.com>
2023-12-09 21:01:34 +0900

    wip

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -3,8 +3,8 @@ import { connectDB } from "@/util/database";
 export default async function handler(req: any, res: any) {
   if (req.method == "GET") {
     const db = (await connectDB).db("forum");
-    let result = await db.collection("post").find().toArray();
-    res.status(200).json(result);
+    let list = await db.collection("post").find().toArray();
+    res.status(200).json(list);
   }
   if (req.method == "POST") {
     return res.status(200).json({ message: "your request = post" });
