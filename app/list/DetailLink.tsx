57b8e92796d959b59a7205a1f8ea8c981435c8fa
cheezeman <pksam5046@naver.com>
2023-12-22 19:37:02 +0900

    WIP : delete a post

--- app/list/DetailLink.tsx
+++ app/list/DetailLink.tsx
@@ -8,12 +8,24 @@ export default function DetailLink(props: any) {
 
   return (
     <div
-      onClick={() => router.push(`/detail/${post._id}`)}
-      className="bg-white rounded-lg p-5 mb-1 shadow-md cursor-pointer"
+      className="bg-white rounded-lg p-5 mb-1 shadow-md"
       key={String(post._id)}
     >
-      <h4 className="text-black text-2xl font-bold m-0">{post.title}</h4>
+      <h4
+        onClick={() => router.push(`/detail/${post._id}`)}
+        className="text-black text-2xl font-bold m-0 cursor-pointer"
+      >
+        {post.title}
+      </h4>
       <p className="text-gray-500 my-1.5">{post.content}</p>
+      <span
+        onClick={() => {
+          fetch("/api/test", { method: "DELETE" }).then(() => {});
+        }}
+        className="text-gray-500 font-bold my-1.5 cursor-pointer"
+      >
+        삭제
+      </span>
     </div>
   );
 }
